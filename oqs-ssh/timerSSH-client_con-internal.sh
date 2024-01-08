a02247faea910466f9a434b64d6734f0dabef006
Kai <adaka.office@gmail.com>
2024-01-08 13:27:04 +0100

    Update timerSSH-client_con-internal.sh

--- oqs-ssh/timerSSH-client_con-internal.sh
+++ oqs-ssh/timerSSH-client_con-internal.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
 /root/oqs-ssh/openssh/ssh -F /root/oqs-ssh/openssh/regress/ssh_config -o KexAlgorithms=curve25519-sha256@libssh.org -o HostKeyAlgorithms=ssh-rsa -o PubkeyAcceptedKeyTypes=ssh-rsa -o PasswordAuthentication=no -i regress/ssh-rsa -t "127.0.0.1" "ip a" 
-#somehost true
