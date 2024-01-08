3c11bbafc11b4fa07eb0419a1db88d86d5cb0e29
Kai <adaka.office@gmail.com>
2024-01-08 13:27:14 +0100

    Update timerSSH-client_con-external.sh

--- oqs-ssh/timerSSH-client_con-external.sh
+++ oqs-ssh/timerSSH-client_con-external.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
 /root/oqs-ssh/openssh/ssh -F /root/oqs-ssh/openssh/regress/ssh_config -o KexAlgorithms=curve25519-sha256@libssh.org -o HostKeyAlgorithms=ssh-rsa -o PubkeyAcceptedKeyTypes=ssh-rsa -o PasswordAuthentication=no -i regress/ssh-rsa -t "192.168.8.206" "ip a" 
-#somehost true
