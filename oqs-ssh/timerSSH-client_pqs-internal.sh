49bbc0b49f898affed420c5f5dc58b338450de14
Kai <adaka.office@gmail.com>
2024-01-08 13:27:35 +0100

    Update timerSSH-client_pqs-internal.sh

--- oqs-ssh/timerSSH-client_pqs-internal.sh
+++ oqs-ssh/timerSSH-client_pqs-internal.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
 /root/oqs-ssh/openssh/ssh -F /root/oqs-ssh/openssh/regress/ssh_config -o KexAlgorithms=kyber-768-sha384 -o HostKeyAlgorithms=ssh-ecdsa-nistp384-dilithium3 -o PubkeyAcceptedKeyTypes=ssh-ecdsa-nistp384-dilithium3 -o PasswordAuthentication=no -i regress/ssh-ecdsa-nistp384-dilithium3 -t "127.0.0.1" "ip a" 
-#somehost true
