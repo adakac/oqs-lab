c9b97988d37553082e08d1d9e901f4962fb105f9
Kai <adaka.office@gmail.com>
2024-01-08 13:15:33 +0100

    Correcting thesis-cloud-ecdh_X25519-local.sh

--- oqs-provider/scripts/thesis-cloud-ecdh_X25519-local.sh
+++ oqs-provider/scripts/thesis-cloud-ecdh_X25519-local.sh
@@ -2,15 +2,6 @@
 
 set -e #instantly quit after error
 
-echo " Cloudflare:"
-(echo -e "GET /cdn-cgi/trace HTTP/1.1\nHost: cloudflare.com\n\n"; echo $'\cc') | openssl s_client -connect pq.cloudflareresearch.com:443 -servername cloudflare.com -groups X25519 -ign_eof 2>/dev/null | grep -E "kex=|Post-Handshake|Cipher"
-echo "------------------------"
-root@kyber-a:~/oqs-provider# ^C
-root@kyber-a:~/oqs-provider# cat scripts/thesis-cloud-ecdh_X25519-local.sh 
-#!/bin/sh
-
-set -e #instantly quit after error
-
 # setup openssl variables
 # -z = length is Zero
 # -n = is Not empty string
